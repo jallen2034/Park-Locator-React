21ef46e4843cbaf426a23b60758d3cb635077309
jallen2034 <jallen2033@gmail.com>
2021-03-30 21:11:43 -0700

    fixed google places API key not working in index.js file and put it in an enviroment variable

--- api/index.js
+++ api/index.js
@@ -1,6 +1,11 @@
+require('dotenv').config()
 const express = require("express");
 const app = express();
 const {Pool} = require('pg');
+
+// https://www.npmjs.com/package/dotenv
+// https://www.npmjs.com/package/@googlemaps/google-maps-services-js
+const {Client} = require("@googlemaps/google-maps-services-js");
 
 // https://node-postgres.com/features/connecting
 const db = new Pool({
@@ -11,8 +16,26 @@ const db = new Pool({
   port: 5432
 });
 
-// connect node to pg db
+// connect my node to pg db
 db.connect();
+
+// instantiate the client to make a call to one of the APIs.
+const client = new Client({});
+
+client
+  .elevation({
+    params: {
+      locations: [{ lat: 45, lng: -110 }],
+      key: process.env.KEY,
+    },
+    timeout: 1000, // milliseconds
+  })
+  .then((r) => {
+    console.log(r.data.results[0].elevation);
+  })
+  .catch((e) => {
+    console.log(e.response.data.error_message);
+  });
 
 // middleware. express.json() allows us to parse json from the client to our back end
 app.use(express.json());
