478c100f75db61edea3816766491ba71671f0a2d
jallen2034 <jallen2033@gmail.com>
2021-04-02 11:52:52 -0700

    fixed bug in my fetch script where it was not populating my tables properly and causing fk conflicts. Had to fix unexpected async behaviour

--- api/db/schema/all_skateparks.sql
+++ api/db/schema/all_skateparks.sql
@@ -1,7 +1,6 @@
 DROP TABLE IF EXISTS all_skateparks;
 
 CREATE TABLE all_skateparks (
-  id SERIAL NOT NULL,
   place_id VARCHAR(255) PRIMARY KEY NOT NULL,
   name VARCHAR(255),
   formatted_address VARCHAR(255),
--- api/db/seeds/fetch.js
+++ api/db/seeds/fetch.js
@@ -1,13 +1,103 @@
 // https://developer.mozilla.org/en-US/docs/Glossary/IIFE
 // inital script to populate skateparks in db - DO NOT RUN THIS SCRIPT IN PRODUCTION
-;(async () => {
+; (async () => {
   require('dotenv').config();
-  const {db, db} = require('../db');
-  const {client} = require('../../mapsApi');
+  const { db } = require('../db');
+  const { client } = require('../../mapsApi');
   await require('../schema/dbReset')();
-  
+
+  /* helper functions
+   * insert allparks information into my all_skateparks table */
+  const insertAllParks = async function (placeDetails, db) {
+    const placeId = placeDetails.result.place_id;
+    const name = placeDetails.result.name;
+    const formattedAddress = placeDetails.result.formatted_address;
+    const phone = placeDetails.result.formatted_phone_number;
+    const website = placeDetails.result.website;
+    const paramaters = [placeId, name, formattedAddress, phone, website];
+
+    const query = `
+          INSERT INTO all_skateparks(
+            place_id,
+            name, 
+            formatted_address, 
+            phone, 
+            website
+          )
+          VALUES ($1, $2, $3, $4, $5)
+        `;
+    await db.query(query, paramaters);
+  }
+
+  const insertReviews = async function (placeDetails, db) {
+    const placeId = placeDetails.result.place_id;
+    const reviews = placeDetails.result.reviews;
+
+    if (reviews) {
+      for await (review of reviews) {
+        const authorName = review.author_name;
+        const authorUrl = review.author_url;
+        const language = review.language;
+        const profileUrl = review.profile_url;
+        const rating = review.rating;
+        const timeDescription = review.relative_time_description;
+        const text = review.text;
+        const time = review.time;
+        const paramaters = [
+          placeId,
+          authorName,
+          authorUrl,
+          language,
+          profileUrl,
+          rating,
+          timeDescription,
+          text,
+          time
+        ];
+
+        const query = `
+            INSERT INTO reviews(
+              place_id, 
+              review_author, 
+              review_author_url, 
+              review_language,
+              review_profile_url,
+              review_rating, 
+              relative_time_desc, 
+              review_text, 
+              review_time
+            )
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+            `;
+        await db.query(query, paramaters);
+      }
+    }
+  }
+
+  // helper to insert types from google api into relevant table
+  const insertTypes = async function (placeDetails, db) {
+    const placeId = placeDetails.result.place_id;
+    const placeType = placeDetails.result.types;
+    console.log("placeId: ", placeId)
+    console.log("placeType array: ", placeType)
+
+    for await (parkType of placeType) {
+      const paramaters = [placeId, parkType];
+
+      const query = `
+          INSERT INTO types(
+            place_id,
+            types
+          )
+          VALUES ($1, $2)
+          `
+
+      await db.query(query, paramaters);
+    }
+  }
+
   // hit API to get back our skateparks
-  client
+  const places = await client
     .placesNearby({
       params: {
         location: { lat: 49.2827, lng: -123.1207 },
@@ -18,58 +108,44 @@
       timeout: 1000, // milliseconds
     })
     .then((r) => {
-      const places  = r.data;
-  
-      /* hit the details endpoint on places api to get more information for each place
-       * https://github.com/googlemaps/google-maps-services-js/blob/master/src/places/details.ts */
-      for (place of places.results) {
-        const myPlaceId = place.place_id;
-        const myFields = [
-          'name',
-          'place_id',
-          'formatted_address', 
-          'formatted_phone_number', 
-          'opening_hours', 
-          'type',
-          'review',
-          'website', 
-          'photo',
-          'geometry'
-        ]
-  
-        client
-          .placeDetails({
-            params: {
-              place_id: myPlaceId,
-              fields: myFields,
-              key: process.env.KEY,
-            },
-            timeout: 1000, // milliseconds
-          })
-          .then((r) => {
-            const placeDetails = r.data;
-            insertAllParks(placeDetails, db);
-          })
-          .catch((e) => {
-            console.log(e.response.data.error_message);
-          });
-      }
+      return r.data;
     })
-  
-    // insert allparks information into my all_skateparks table
-    const insertAllParks = function(placeDetails, db) {
-      const placeId = placeDetails.result.place_id;
-      const name = placeDetails.result.name;
-      const formattedAddress = placeDetails.result.formatted_address;
-      const phone = placeDetails.result.formatted_phone_number;
-      const website = placeDetails.result.website;
-      const paramaters = [placeId, name, formattedAddress, phone, website];
-  
-      const query = `
-        INSERT INTO all_skateparks(place_id, name, formatted_address, phone, website)
-        VALUES ($1, $2, $3, $4, $5)
-      `;
-      db.query(query, paramaters);
-    }
-    db.end()
+
+  /* hit the details endpoint on places api to get more information for each place
+   * https://github.com/googlemaps/google-maps-services-js/blob/master/src/places/details.ts */
+  for (place of places.results) {
+    const myPlaceId = place.place_id;
+    const myFields = [
+      'name',
+      'place_id',
+      'formatted_address',
+      'formatted_phone_number',
+      'opening_hours',
+      'type',
+      'review',
+      'website',
+      'photo',
+      'geometry'
+    ]
+
+    const placeDetails = await client
+      .placeDetails({
+        params: {
+          place_id: myPlaceId,
+          fields: myFields,
+          key: process.env.KEY,
+        },
+        timeout: 1000, // milliseconds
+      })
+      .then((r) => {
+        return r.data;
+      })
+      .catch((e) => {
+        console.log(e.response.data.error_message);
+      });
+
+    await insertAllParks(placeDetails, db);
+    await insertTypes(placeDetails, db);
+    await insertReviews(placeDetails, db);
+  }
 })()
