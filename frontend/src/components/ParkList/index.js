472a49e56812bbbe952899712cf5e2117e1c831b
jallen2034 <jallen2033@gmail.com>
2021-04-27 14:07:06 -0700

    got side div to highlight when a div and marker is clicked

--- frontend/src/components/ParkList/index.js
+++ frontend/src/components/ParkList/index.js
@@ -7,14 +7,31 @@ const listItemClick = function (event, place_id, setClickedPark) {
   setClickedPark(place_id)
 }
 
+const useStyles = makeStyles((theme) => ({
+  selected: {
+    backgroundColor: '#e6e6e6'
+  },
+  notSelected: {
+    backgroundColor: 'white'
+  }
+}))
+
 // loop through array of data and create a parklistitem with prop info for each park
 const ParkList = ({ parksForMap, currentUser, setClickedPark, clickedPark }) => {
+  const classes = useStyles()
   let listOfParks
-
+  
   if (parksForMap.length > 0) {
     listOfParks = parksForMap.map(({ place_id, name, formatted_address, phone, website }) => {
+      let selected = false
+      if (clickedPark === place_id) selected = true
+      
       return (
-        <div place_id={place_id} onClick={(event) => listItemClick(event, place_id, setClickedPark)}>
+        <div 
+          place_id={place_id} 
+          onClick={(event) => listItemClick(event, place_id, setClickedPark)}
+          className={(selected === true) ? classes.selected : classes.notSelected}
+          >
           <ParkListItem
             place_id={place_id}
             name={name}
