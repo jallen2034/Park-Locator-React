5039143b538dfeb5f9e827ba5d30c3c2409e50a1
jallen2034 <jallen2033@gmail.com>
2021-04-27 15:53:48 -0700

    implemtned functionality where the user can click on a map marker for a park and it auto scrolls to the park in the parklist and highlights it

--- frontend/src/components/ParkListItem/index.js
+++ frontend/src/components/ParkListItem/index.js
@@ -1,17 +1,25 @@
-import React from 'react'
+import { useEffect, useRef } from 'react'
 import ParkData from '../ParkData'
 import Button from '../Button'
 
-const ParkListItem = ({place_id, name, formattedAddress, phone, website, currentUser}) => {
+const ParkListItem = ({ place_id, name, formattedAddress, phone, website, currentUser, selected }) => {
+  const parkDiv = useRef(null)
+
+  useEffect(() => {
+    if (parkDiv.current && selected) {
+      parkDiv.current.scrollIntoView({ behavior: 'smooth'})
+    }
+  }, [selected, parkDiv])
+
   return (
-    <div id={place_id}>
+    <div id={place_id} ref={parkDiv}>
       <ParkData
         name={name}
         formattedAddress={formattedAddress}
         phone={phone}
         website={website}
       />
-      <Button place_id={place_id} currentUser={currentUser}/>
+      <Button place_id={place_id} currentUser={currentUser} />
     </div>
   )
 }
