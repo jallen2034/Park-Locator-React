3a1dfef556cc535ed481d00953a56f5ed9ae95ad
jallen2034 <jallen2033@gmail.com>
2021-05-02 21:34:58 -0700

    added some styling to the data from our API for the all reviews page

--- frontend/src/components/IndividualPark/index.js
+++ frontend/src/components/IndividualPark/index.js
@@ -1,21 +1,51 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography'
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    padding: '25px',
-    marginTop: '40px'
-  }
+    paddingLeft: '25px',
+    paddingRight: '25px',
+    marginTop: '20px',
+    marginBottom: '20px',
+    borderRadius: 12
+  },
+  h4: {
+    fontSize: '26px',
+  },
+  h5: {
+    fontSize: '18px',
+    fontWeight: 'bold',
+    marginTop: '10px',
+    marginBottom: '10px'
+  },
+  h6: {
+    fontSize: '16px',
+    marginTop: '10px',
+    marginBottom: '10px'
+  },
 }))
 
 // child component to handle an individual review 
 const IndividualReview = ({ review }) => {
+  const classes = useStyles()
+
   return (
-    <>
-      <h4>{review.review_author}</h4>
-      <h4>{review.review_rating}</h4>
-      <h4>{review.review_text}</h4>
-    </>
+    <Card variant="outlined" className={classes.root}>
+      <CardContent>
+        <Typography variant='h5' className={classes.h5}>
+          {review.review_author}
+        </Typography>
+        <Typography variant='h6' className={classes.h6}>
+          {review.review_rating}
+        </Typography>
+        <Typography variant='h6' className={classes.h6}>
+          {review.review_text}
+        </Typography>
+      </CardContent>
+    </Card>
   )
 }
 
@@ -28,7 +58,9 @@ const IndividualPark = ({ park, value }) => {
 
   return (
     <div className={classes.root}>
-      <h2>{park}</h2>
+       <Typography variant='h4' className={classes.h4}>
+         {park}
+       </Typography>
       {individualParkReviews}
     </div>
   )
--- frontend/src/components/ReviewParkList/index.js
+++ frontend/src/components/ReviewParkList/index.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import IndividualPark from '../IndividualPark'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: '90px',
+  }
+}))
 
 // component to render out all the reviews for parks
 const ReviewParkList = ({ reviews }) => {
+  const classes = useStyles()
   const parkReviews = [] 
 
   for (const [park, value] of Object.entries(reviews)) {
@@ -16,8 +24,10 @@ const ReviewParkList = ({ reviews }) => {
     ) 
   }
 
+  console.log("parkReviews array of reviews: ", parkReviews)
+
   return (
-    <div>
+    <div className={classes.root}>
       {parkReviews}
     </div>
   )
