852f50547bff6337731def1cb19f198989be2bf8
jallen2034 <jallen2033@gmail.com>
2021-05-06 13:08:57 -0700

    added a working feature where the user clicks on a park, the iconn for the park selected changed from blue to red conditionally

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,7 @@ import React, { useState } from 'react'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import UsersParksPage from '../src/components/UsersParksPage'
-import ReviewPage from '../src/components/ReviewPage'
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // app component
 function App() {
--- frontend/src/components/Map/index.js
+++ frontend/src/components/Map/index.js
@@ -1,4 +1,6 @@
+import { useState } from 'react'
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
+import * as L from "leaflet";
 import React from 'react'
 
 /* helper to set the users selected park/marker 
@@ -17,18 +19,33 @@ function ChangeView({ center, zoom }) {
 }
 
 /* https://react-leaflet.js.org/docs/start-introduction
+ * https://codewithwolf.com/how-to-change-marker-color-react-leaflet
  * https://github.com/PaulLeCam/react-leaflet/issues/453
  * https://stackoverflow.com/questions/40901539/arbitrary-function-on-react-leaflet-marker-click
  * https://react-leaflet.js.org/docs/api-map */
-const MapDisplay = ({ parksForMap, setClickedPark, clickedPark, mapCenter, setMapCenter, setClickedParkInList }) => {
+const MapDisplay = ({ parksForMap, setClickedPark, clickedPark, mapCenter, setMapCenter, clickedParkInList, setClickedParkInList }) => {
+  //  Create the Icon
+  const LeafIcon = L.Icon.extend({
+    options: {}
+  });
+  const blueIcon = new LeafIcon({
+    iconUrl:
+      "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png"
+  })
+  const redIcon = new LeafIcon({
+    iconUrl:
+      "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png"
+  })
   let listOfMarkers
 
+  // map through and render markers and also conditonally set the clicked icon to red
   if (parksForMap.length > 0) {
-
     listOfMarkers = parksForMap.map(({ place_id, location_lat, location_long }) => {
+
       return (
-        <Marker 
-          place_id={place_id} 
+        <Marker
+          place_id={place_id}
+          icon={place_id === clickedPark || place_id === clickedParkInList ? redIcon : blueIcon}
           position={[location_lat, location_long]}
           eventHandlers={{
             click: (event) => {
@@ -42,13 +59,13 @@ const MapDisplay = ({ parksForMap, setClickedPark, clickedPark, mapCenter, setMa
   }
 
   return (
-    <MapContainer 
-      center={mapCenter} 
-      zoom={13} 
-      scrollWheelZoom={true} 
-      style={{ height: '100vh', width: '70vw', marginLeft: '0px', overflow: 'hidden'}}
+    <MapContainer
+      center={mapCenter}
+      zoom={13}
+      scrollWheelZoom={true}
+      style={{ height: '100vh', width: '70vw', marginLeft: '0px', overflow: 'hidden' }}
     >
-      <ChangeView center={mapCenter} zoom={13} /> 
+      <ChangeView center={mapCenter} zoom={13} />
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=lVUjykFYNWAQcw6IhXoj"
